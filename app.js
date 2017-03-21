e7b2baf4ab5f01504c7fdd66dd208c71311367a5
Deshun Cai <unusep@gmail.com>
2017-03-21 21:58:00 +0800

    Add body parser

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const botToken = process.env.BOT_TOKEN;
 
+function getUpdate(req){
+    return req.body;
+}
+function getCommand(update){
+    const text = update.message.text;
+    if (!!text){
+        return text.split(" ");
+    } else {
+        return [];
+    }
+}
+
 const handleUpdate = (req, res) => {
     const update = getUpdate(req);
-    const command = getCommand(update);
+    const [command, ...args] = getCommand(update);
+    console.log(command);
     switch (command) {
         case "remind":
             break;
         default:
             // handle
     }
+    res.sendStatus(200);
 }
 
 app.post(`/{bot_Token}`, handleUpdate);
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!');
 })
