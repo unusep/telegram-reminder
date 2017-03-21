ee81751eda5a135633df3c916c51bdb6ae1dcff1
Deshun Cai <unusep@gmail.com>
2017-03-21 21:18:46 +0800

    Add env file and handle updates

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
 require('dotenv').config();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+const botToken = process.env.BOT_TOKEN;
+
+const handleUpdate = (req, res) => {
+    const update = getUpdate(req);
+    const command = getCommand(update);
+    switch (command) {
+        case "remind":
+            break;
+        default:
+            // handle
+    }
+}
+
+app.post(`/{bot_Token}`, handleUpdate);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('Example app listening on port 3000!')
 })
